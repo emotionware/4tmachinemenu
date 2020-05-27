82a57d7b9b7fb1aa61ea1927ffb02abcc790a0ae
emotionware <fernando.castro@emotionware.net>
2020-05-26 22:54:05 -0500

    ok

--- _4TPyModel/templates/config/diagramitems.js
+++ _4TPyModel/templates/config/diagramitems.js
@@ -24,31 +24,31 @@ if (diagrama=='menu'){
     
         
    if (item==18){
-        window.open(window.location.protocol + '//' + window.location.hostname + ':5001','_blank');
+        window.open(window.location.protocol + '//' + '127.0.0.1' + ':5000','_blank');
     }
     
    if (item==19){
-        window.open(window.location.protocol + '//' + window.location.hostname + ':5002','_blank');
+        window.open(window.location.protocol + '//' + '127.0.0.2' + ':5000','_blank');
     }
     
    if (item==20){
-        window.open(window.location.protocol + '//' + window.location.hostname + ':5003','_blank');
+        window.open(window.location.protocol + '//' + '127.0.0.3' + ':5000','_blank');
     }
     
    if (item==23){
-        window.open(window.location.protocol + '//' + window.location.hostname + ':5004','_blank');
+        window.open(window.location.protocol + '//' + '127.0.0.3' + ':5000','_blank');
     }    
     
    if (item==24){
-        window.open(window.location.protocol + '//' + window.location.hostname + ':5005','_blank');
+        window.open(window.location.protocol + '//' + '127.0.0.5' + ':5000','_blank');
     }    
     
    if (item==27){
-        window.open(window.location.protocol + '//' + window.location.hostname + ':5007','_blank');
+        window.open(window.location.protocol + '//' + '127.0.0.7' + ':5000','_blank');
     }       
     
    if (item==26){
-        window.open(window.location.protocol + '//' + window.location.hostname + ':5006','_blank');
+        window.open(window.location.protocol + '//' + '127.0.0.6' + ':5000','_blank');
     }       
         
          if (item==22){
